39fd739924c4ba9d420512e9c6a3d15201563be5
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-05-23 14:53:05 -0300

    Criando array de temas e mostrando eles em um componente Tema

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.css";
 import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
+import { Tema } from "./componentes/Tema";
 
 const App = () => {
+  const temas = [
+    {
+      id: 1,
+      nome: "front-end",
+    },
+    {
+      id: 2,
+      nome: "backend",
+    },
+    {
+      id: 3,
+      nome: "devops",
+    },
+    {
+      id: 4,
+      nome: "inteligência artificial",
+    },
+    {
+      id: 5,
+      nome: "data science",
+    },
+    {
+      id: 6,
+      nome: "cloud",
+    },
+  ];
+
   return (
     <main>
       <header>
@@ -11,6 +39,14 @@ const App = () => {
         <img src="/banner.png" alt="" />
       </section>
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]} />
+        <Tema tema={temas[1]} />
+        <Tema tema={temas[2]} />
+        <Tema tema={temas[3]} />
+        <Tema tema={temas[4]} />
+        <Tema tema={temas[5]} />
+      </section>
     </main>
   );
 };
---
+++ src/componentes/Tema/index.jsx
@@ -0,0 +1,5 @@
+import "./tema.estilos.css";
+
+export const Tema = ({ tema }) => {
+  return <h3>{tema.nome}</h3>;
+};
